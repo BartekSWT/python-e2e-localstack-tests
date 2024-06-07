6101eda22ba124699c52178f69fb6a8b8e774ea6
Bartek <tyszkiewicz.bartosz@gmail.com>
2024-06-07 12:46:50 +0200

    added method for last_name and first_name in user_generator to make sure they have at least 4 letters, like username and password

--- generators/user_generator.py
+++ generators/user_generator.py
@@ -4,6 +4,7 @@ from api.data.register import User
 
 fake = Faker()
 
+
 def generate_username() -> str:
     username = fake.user_name()
     attempts = 0
@@ -11,6 +12,7 @@ def generate_username() -> str:
         username = fake.user_name()
         attempts += 1
     return username
+
 
 def generate_password() -> str:
     password = fake.password()
@@ -20,15 +22,36 @@ def generate_password() -> str:
         attempts += 1
     return password
 
+
+def generate_first_name() -> str:
+    first_name = fake.first_name()
+    attempts = 0
+    while len(first_name) < 4 and attempts < 10:
+        first_name = fake.first_name()
+        attempts += 1
+    return first_name
+
+
+def generate_last_name() -> str:
+    last_name = fake.last_name()
+    attempts = 0
+    while len(last_name) < 4 and attempts < 10:
+        last_name = fake.last_name()
+        attempts += 1
+    return last_name
+
+
 def get_random_user() -> User:
     username = generate_username()
     password = generate_password()
+    first_name = generate_first_name()
+    last_name = generate_last_name()
 
     return User(
         username=username,
         password=password,
         email=fake.email(),
-        firstName=fake.first_name(),
-        lastName=fake.last_name(),
+        firstName=first_name,
+        lastName=last_name,
         roles=["ROLE_ADMIN", "ROLE_CLIENT"],
     )
