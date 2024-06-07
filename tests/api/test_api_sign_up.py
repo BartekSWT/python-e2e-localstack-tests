a256313c09649e0157d4801861e08b8988d65482
Bartek <tyszkiewicz.bartosz@gmail.com>
2024-06-07 10:32:05 +0200

    new test for sign_up

---
+++ tests/api/test_api_sign_up.py
@@ -0,0 +1,27 @@
+import random
+import pytest
+import os
+from api.post_sign_up import SignUp
+from dotenv import load_dotenv
+import requests
+from faker import Faker
+from generators import user_generator
+
+load_dotenv()
+
+
+@pytest.fixture
+def sign_up_api():
+    return SignUp()
+
+
+def test_successful_sign_up(sign_up_api: SignUp):
+    fake = Faker()
+    user = user_generator.get_random_user()
+    response = sign_up_api.api_call(user)
+    try:
+        response.raise_for_status()
+        assert response.status_code == 201, "Expected status code 201"
+        assert response.json()['token'] is not None, "Token should not be None"
+    except requests.exceptions.HTTPError as e:
+        pytest.fail(f"HTTPError occurred: {str(e)}")
